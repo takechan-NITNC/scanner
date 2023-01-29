2c180553fab4ef6b26d604fd713354a1ff6f4b74
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-01-29 17:55:58 +0900

    Markdownの名残を削除

--- scanner.h
+++ scanner.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdio.h>
-int has_next(FILE\*);
-char *next(FILE\*);
-int next_int(FILE\*);
-int next_double(FILE\*);
-char *next_line(FILE\*);
+int has_next(FILE*);
+char *next(FILE*);
+int next_int(FILE*);
+int next_double(FILE*);
+char *next_line(FILE*);
